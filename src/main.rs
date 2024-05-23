4e465b1d44d6e025e6d4cc7916ebe20cfca2c990
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2024-05-23 14:44:57 -0600

    Adjusted Separators
    Separators now adjust to the terminal width.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use battery::Manager;
+use battery as battery_crate;
+use battery_crate::Manager;
 use uom::si::time::second;
 use uom::si::f32::Time;
+use term_size;
 
-fn main() -> Result<(), battery::Error> {
+fn main() -> Result<(), battery_crate::Error> {
     let manager = Manager::new()?;
+    let terminal_width = term_size::dimensions().map_or(40, |(w, _)| w); 
 
     for (_idx, maybe_battery) in manager.batteries()?.enumerate() {
         let battery = maybe_battery?;
-        println!("========================================");
-        println!("Battery: {:.2}%", battery.state_of_charge().get::<battery::units::ratio::percent>());
+        let separator = "=".repeat(terminal_width);
+        println!("{}", separator);
+        println!("Battery: {:.2}%", battery.state_of_charge().get::<battery_crate::units::ratio::percent>());
         println!("{:?}", battery.state());
         if let Some(time_to_full) = battery.time_to_full() {
             println!("Time until fully charged: {}", format_duration(time_to_full));
         } else {
             println!("Time until fully charged: N/A");
         }
-        println!("========================================");
+        println!("{}", separator);
         println!();
     }
 
